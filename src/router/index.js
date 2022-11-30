04809115db72f31cd5671498f545bec70bef9945
belkisarslan <belkisarslan33@gmail.com>
2022-11-30 13:22:23 +0300

    general cleaning done

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,15 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/add',
+    name: 'AddProject',
+    component: () => import('../views/AddProject.vue')
+  },
+  {
+    path: '/projects/:id',
+    name: 'EditProject',
+    component: () => import('../views/EditProject.vue'),
+    props: true
   }
 ]
 
