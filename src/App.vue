2fd7895de65b012dbc9340da916256f6237f7906
belkisarslan <belkisarslan33@gmail.com>
2022-12-01 15:58:38 +0300

    navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/add">AddProject</router-link>
-  </div>
+  <Navbar/>
   <router-view/>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue"
 export default{
   components:{
-
+   Navbar
   }
 }
 </script>
